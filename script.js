eb364326c8fe6e09ed54c9ac4e51c586d505939b
bscript <bscript@gmail.com>
2023-12-21 23:17:07 +0500

    third

--- script.js
+++ script.js
@@ -1,42 +1,63 @@
 
 
-    let name = prompt('Введите свое имя')
-    let year = +prompt( ' Введите нынешний год')
-    let age = +prompt(' Введите год рождения')
+//     let name = prompt('Введите свое имя')
+//     let year = +prompt( ' Введите нынешний год')
+//     let age = +prompt(' Введите год рождения')
 
 
-function about( years, ages) {
+// function about( years, ages) {
     
-    let num = years - ages
-    return num
-}
-console.log( 'Вас зовут ' + name + ' Вам ' + about(year, age) + ' лет');
+//     let num = years - ages
+//     return num
+// }
+// console.log( 'Вас зовут ' + name + ' Вам ' + about(year, age) + ' лет');
 
 
-let math = prompt( ' сколько примеров вы хотите ?')
+// let math = prompt( ' сколько примеров вы хотите ?')
 
 
 
 function oper() {
+        return Math.floor(Math.random() * 4 + 1)
+}
+
+
+
+if (oper() == 1) {
+
+    oper() = '+'
     
+} else if ( oper() == 2) {
+
+    oper() = '*'
+} else if ( oper() == 3){
+    oper() = '*'
+}else if ( oper() == 4) {
+    oper() = '/'
 }
 
 
 
+console.log(oper());
 
 
-function solve( ) {
-
-    return Math.floor(Math.random() * 50 + 1)
-}
 
 
-for (let i = 0; i < math; i++) {
-    let first = solve()
-    let second = solve()
-    let answer = +prompt(first + ' + ' + second + ' = ? ')
-    let trueAnswer = first + second
-    console.log(answer == trueAnswer ? ' Ваш ответ верный : ' + answer :  ' Ваш ответ не верный ' + answer + ' Правильный ответ ' + trueAnswer ) 
-}
 
-alert(' Откройте панель разработчика чтобы увидеть результаты')
+
+
+// function solve( ) {
+
+//     return Math.floor(Math.random() * 50 + 1)
+// }
+
+
+// for (let i = 0; i < math; i++) {
+//     let first = solve()
+//     let second = solve()
+//     let answer = +prompt(first + ' + ' + second + ' = ? ')
+//     let trueAnswer = first + second
+//     console.log(answer == trueAnswer ? ' Ваш ответ верный : ' + answer :  ' Ваш ответ не верный ' + answer + ' Правильный ответ ' + trueAnswer ) 
+// }
+
+// alert(' Откройте панель разработчика чтобы увидеть результаты')
